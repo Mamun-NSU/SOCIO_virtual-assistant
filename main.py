2d02196d8e4b020207b482c03b5e5071130d07c8
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2021-02-25 23:21:53 +0600

    SOCIO
    SOCIO code

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+socio = pyttsx3.init()
+voices = socio.getProperty('voices')
+socio.setProperty('voices', voices[1].id)
+socio.say('How can i help you')
+socio.runAndWait()
 
 try:
     with sr.Microphone() as source:
